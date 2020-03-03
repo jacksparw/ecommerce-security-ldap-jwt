24b39b54abee317a83adbaa1a4485276d4861921
Himanshu Tomar <himanshu.tomar@netent.com>
2020-03-03 19:36:42 +0530

    eureka client dependency

--- src/main/java/com/ecommerce/SecurityService/SecurityServiceApplication.java
+++ src/main/java/com/ecommerce/SecurityService/SecurityServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.SecurityService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SecurityServiceApplication {
 
 	public static void main(String[] args) {
